d9b67d3b43a5f09f8c1d4821c00cd2bb7264d211
= <=>
2017-04-14 13:34:30 -0400

    changed the name of the loader; setup entrypoints and dependencies

---
+++ python/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+'''
+
+
+Created on  2017-04-12 16:13:05
+
+@author: =
+@copyright: 2016 The Presidents and Fellows of Harvard College. All rights reserved.
+@license: GPL v2.0
+'''
+
+from setuptools import setup, find_packages
+
+setup(
+    name="seqdb",
+    version="0.1.0",
+    author='Aaron Kitzmiller <aaron_kitzmiller@harvard.edu>',
+    author_email='aaron_kitzmiller@harvard.edu',
+    description='A BioSQL sequence database loader',
+    license='LICENSE.txt',
+    url='http://pypi.python.org/pypi/seqdb/',
+    packages=find_packages(),
+    long_description='Harvard Annotator',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+    ],
+    entry_points={
+        'console_scripts': [
+            'seqdb-loader = seqdb.seqdb-loader:main'
+        ]
+    },
+    install_requires=[
+        'biopython>=1.69',
+        'MySQL-python>=1.2.5',
+    ],
+)
